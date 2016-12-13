e9355a6075b322f2973901e94fc52f9b010d5526
Ebubekir Ogden <ebubekirogden@gmail.com>
2016-12-13 14:52:25 -0800

    C-Socket

---
+++ TCP_client.c
@@ -0,0 +1,24 @@
+//
+//  TCP_client.c
+//  
+//
+//  Created by Ebubekir on 11/19/16.
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+int main(int argc, char *argv[]){
+    int network_socket;
+    network_socket = socket(AF_INET, SOCK_STREAM, )
+    
+    
+    return 0;
+}
+
